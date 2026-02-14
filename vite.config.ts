4a65cddb3fbcc407824d6b225564e2231ce3aed8
pkdaigo <pkdaigo@gmail.com>
2026-02-14 11:34:32 +1000

    Fix: Separate React into its own chunk to fix loading order

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,18 @@ export default defineConfig({
     minify: 'esbuild',
     sourcemap: false,
     target: 'es2020',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
   preview: {
     port: 4173,
